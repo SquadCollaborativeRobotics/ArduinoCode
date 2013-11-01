b395eee10cc04615ab792cd7498ccd94f58165ba
Alex Sher <alexbsher@gmail.com>
2013-11-01 16:58:56 -0400

    scr_arduino update

--- sketchbook/scr_arduino/scr_arduino.ino
+++ sketchbook/scr_arduino/scr_arduino.ino
@@ -1,40 +1,51 @@
 /*
- * ros
- * Prints "hello world!"
+ * Squad Collaborative Robotics
+ * Prototype Embedded Collector Bot Code
  */
 
 #include <ros.h>
 #include <std_msgs/Int32.h>
-#include <scr_proto/DiffCommand.h>
+//#include <PololuQik.h>
+//#include <SoftwareSerial.h>
 
-// Placeholders for
+// Placeholders for motor commands
 int lm_cmd = 0;
 int rm_cmd = 0;
 
-void (const std_msgs::Int32& diff_msg){
-  lm = diff_msg.data;
+// Motor Callbacks
+void leftMotorCallback(const std_msgs::Int32& lm_msg){
+  lm_cmd = lm_msg.data;
+}
+
+void rightMotorCallback(const std_msgs::Int32& rm_msg){
+  rm_cmd = rm_msg.data;
 }
 
 
 ros::NodeHandle  nh;
-
 std_msgs::Int32 int_msg;
-ros::Publisher chatter("chatter", &int_msg);
-ros::Subscriber<std_msgs::Int32> DiffSub("motor_com", diffCb);
+ros::Subscriber<std_msgs::Int32> leftMotorSub("left_motor", leftMotorCallback);
+ros::Subscriber<std_msgs::Int32> rightMotorSub("right_motor", rightMotorCallback);
 
-
+// Rx, Tx, Reset Pins
+//PololuQik2s12v10 pmd(2, 3, 4);
 
 void setup()
 {
+  // ROS Stuff
   nh.initNode();
-  nh.advertise(chatter);
-  nh.subscribe(DiffSub);
+  nh.subscribe(leftMotorSub);
+  nh.subscribe(rightMotorSub);
+  
+  // Motor Driver Init
+  //pmd.init();
 }
 
 void loop()
 {
-  int_msg.data = lm;
+  int_msg.data = lm_cmd;
   chatter.publish( &int_msg );
   nh.spinOnce();
-  delay(10);
+  //pmd.setSpeeds(lm_cmd, rm_cmd);
+  delay(5);
 }
