027ed1a783f56b41e78bc5c6cdecf41f0dfe7c15
Alex Sher <alexbsher@gmail.com>
2013-11-28 13:50:31 -0600

    Cleaned up a little bit and fixed odometry errors.

--- scr_arduino/scr_arduino.ino
+++ scr_arduino/scr_arduino.ino
@@ -1,7 +1,7 @@
 /*
  * Squad Collaborative Robotics
  * Prototype Embedded Collector Bot Code
-   Arduino Mega 2560 R3
+ * Arduino Mega 2560 R3
  */
 
 // ROS Includes
@@ -22,8 +22,7 @@
 #define LEFT_ENCODER_B_PIN 4
 #define RIGHT_ENCODER_A_PIN 3
 #define RIGHT_ENCODER_B_PIN 5
-#define ENCODER_TIME_DELAY_MS 100
-#define TICKS_PER_REVOLUTION 750
+#define TICKS_PER_REVOLUTION 3200
 #define PI 3.141592654
 
 // Pololu Motor Driver Pins
@@ -35,8 +34,8 @@
 ros::NodeHandle_<ArduinoHardware, 5, 5, 256, 256>  nh;
 
 // Encoder Messages
-std_msgs::Int32 l_enc_msg;
-std_msgs::Int32 r_enc_msg;
+// std_msgs::Int32 l_enc_msg;
+// std_msgs::Int32 r_enc_msg;
 
 // Wheel Speed Messages
 std_msgs::Float32 l_wheel_msg;
@@ -47,8 +46,8 @@ scr_proto::SpeedCommand motor_msg;
 
 // Initialize Publishers
 //ros::Publisher left_encoder_pub("left_encoder", &l_enc_msg);
-ros::Publisher left_wheel_pub("lw_speed", &l_wheel_msg);
 //ros::Publisher right_encoder_pub("right_encoder", &l_enc_msg);
+ros::Publisher left_wheel_pub("lw_speed", &l_wheel_msg);
 ros::Publisher right_wheel_pub("rw_speed", &r_wheel_msg);
 
 // Placeholders for wheel speeds in rad/s
@@ -63,20 +62,29 @@ double rm_cmd = 0;
 double lw_cmd_spd = 0;
 double rw_cmd_spd = 0;
 
-// Encoder initialization
+// Encoder object initialization
 Encoder L_DCMotorEncoder(LEFT_ENCODER_A_PIN, LEFT_ENCODER_B_PIN);
 Encoder R_DCMotorEncoder(RIGHT_ENCODER_A_PIN, RIGHT_ENCODER_B_PIN);
 
+// Encoder value initialization
 long last_encoder_time = micros();  // not the best way to get this done... should check existence in Update Wheel Function
+
+// Left Encoder
+long L_EncoderValue = 0;
 float L_EncoderVelocity = 0; // Radians per second
 long L_LastEncoderValue = 0;
 double L_EncoderDelta = 0; 
 double L_LastEncoderDelta = 0; 
+
+// Right Encoder
+long R_EncoderValue = 0;
 float R_EncoderVelocity = 0; // Radians per second
 long R_LastEncoderValue = 0;
 double R_EncoderDelta = 0; 
 double R_LastEncoderDelta = 0; 
-double beta = 0.0;  // Low Pass Parameter for encoder velocity
+
+// Low Pass Parameter for encoder velocity
+double beta = 0.0;  
 
 // Speed Controller Initialization
 double L_PIDout = 0;
@@ -105,34 +113,32 @@ void MotorCallback(const scr_proto::SpeedCommand& motor_com){
   rw_cmd_spd = motor_com.right_motor_w;
 }
 
+// Subscriber to speed commands on computer
+ros::Subscriber<scr_proto::SpeedCommand> motor_sub("speed_command", MotorCallback);
+
 void updateEncoderReading() {
   // Get raw ticks
-  long L_encoderValue = L_DCMotorEncoder.read();
-  long R_encoderValue = R_DCMotorEncoder.read();
+  L_EncoderValue = L_DCMotorEncoder.read();
+  R_EncoderValue = R_DCMotorEncoder.read();
   
+  // Current time in micro seconds
   long now = micros();
   
   long dt = (now - last_encoder_time);
   
-  // Get position in revolutions (includes multiple revolutions)
-// Serial.println(encoderValue);
-  L_EncoderDelta = (double)(L_encoderValue - L_LastEncoderValue) * (1-beta) + L_LastEncoderDelta * beta;
-  R_EncoderDelta = (double)(R_encoderValue - R_LastEncoderValue) * (1-beta) + R_LastEncoderDelta * beta;
-// Serial.println(g_EncoderAngle);
+  // Get position in revolutions (includes multiple revolutions) with low pass filter
+  L_EncoderDelta = (double)(L_EncoderValue - L_LastEncoderValue) * (1-beta) + L_LastEncoderDelta * beta;
+  R_EncoderDelta = (double)(R_EncoderValue - R_LastEncoderValue) * (1-beta) + R_LastEncoderDelta * beta;
   
-  // Get velocity in ticks/sec
-  L_EncoderVelocity = 1000000.0*(double)(L_EncoderDelta)/dt;
-  R_EncoderVelocity = 1000000.0*(double)(R_EncoderDelta)/dt;
-
-  // Convert to revolutions/sec
-  L_EncoderVelocity /= (double)TICKS_PER_REVOLUTION;
-  R_EncoderVelocity /= (double)TICKS_PER_REVOLUTION;
+  // Get velocity in revs/sec
+  L_EncoderVelocity = 1000000.0/dt * L_EncoderDelta/TICKS_PER_REVOLUTION;
+  R_EncoderVelocity = 1000000.0/dt * R_EncoderDelta/TICKS_PER_REVOLUTION;
   
-  // Convert to rad/s
+  // Convert to rad/sec
   L_WheelVelocity = L_EncoderVelocity * 2 * PI;
   R_WheelVelocity = R_EncoderVelocity * 2 * PI;
 
-  // Set last known encoder ticks
+  // Update previous values
   L_LastEncoderValue = L_encoderValue;
   R_LastEncoderValue = R_encoderValue;
 
@@ -142,9 +148,9 @@ void updateEncoderReading() {
   R_LastEncoderDelta = R_EncoderDelta;
 }
 
-ros::Subscriber<scr_proto::SpeedCommand> motor_sub("speed_command", MotorCallback);
 
 
+// Pololu Motor Driver Initialization
 // Rx, Tx, Reset Pins
 PololuQik2s12v10 pmd(PMD_RX, PMD_TX, PMD_RESET);
 
@@ -161,8 +167,8 @@ void setup()
   nh.subscribe(motor_sub);
   
   // Encoder Pubs
-  //nh.advertise(left_encoder_pub);
-  //nh.advertise(right_encoder_pub);
+  // nh.advertise(left_encoder_pub);
+  // nh.advertise(right_encoder_pub);
   
   // Wheel Speeds in rad/s
   nh.advertise(right_wheel_pub);
@@ -194,14 +200,14 @@ void loop()
   rm_cmd += R_PIDout;
 
   // Populate messages
-  //l_enc_msg.data = lm_cmd;
-  //r_enc_msg.data = rm_cmd;
+  // l_enc_msg.data = L_encoderValue;
+  // r_enc_msg.data = rm_cmd;
   l_wheel_msg.data = L_WheelVelocity;
   r_wheel_msg.data = R_WheelVelocity;
 
   // Publish Data
-  //left_encoder_pub.publish( &l_enc_msg );
-  //right_encoder_pub.publish( &r_enc_msg );
+  // left_encoder_pub.publish( &l_enc_msg );
+  // right_encoder_pub.publish( &r_enc_msg );
   left_wheel_pub.publish( &l_wheel_msg );
   right_wheel_pub.publish( &r_wheel_msg );
   
